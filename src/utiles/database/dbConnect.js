e56e8d1f8819a6312e974296bb5c58c6e3663bf4
winchester-cell <manili.mm91@gmail.com>
2025-07-14 13:53:20 +0330

    dbchanged

--- src/utiles/database/dbConnect.js
+++ src/utiles/database/dbConnect.js
@@ -1,18 +1,37 @@
+// import mongoose from "mongoose";
+
+// const dbConnect = async () => {
+//     try {
+//         if (mongoose.connections[0].readyState) {
+//             return false
+//         } else {
+//             await mongoose.connect('mongodb+srv://manilimm91:reza3663@cluster0.u2mhath.mongodb.net/todoproject?retryWrites=true&w=majority&appName=Cluster0')
+//             console.log('Connected to DataBase');
+//         }
+//     } catch (err) {
+//         console.log('Error =>', err);
+//     }
+// }
+
+// export default dbConnect
+
+
 import mongoose from "mongoose";
 
 const dbConnect = async () => {
-    try {
-        if (mongoose.connections[0].readyState) {
-            return false
-        } else {
-            // mongoose.connect('mongodb://localhost:27017/TodoList')
-            // mongoose.connect('mongodb+srv://manilimm91:reza3663@cluster0.u2mhath.mongodb.net/TodoProject')
-            mongoose.connect('mongodb+srv://manilimm91:reza3663@cluster0.u2mhath.mongodb.net/todoproject?retryWrites=true&w=majority&appName=Cluster0')
-            console.log('Connected to DataBase');
-        }
-    } catch (err) {
-        console.log('Error =>', err);
-    }
-}
+  if (mongoose.connections[0].readyState) {
+    return;
+  }
 
-export default dbConnect
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+  }
+};
+
+export default dbConnect;
