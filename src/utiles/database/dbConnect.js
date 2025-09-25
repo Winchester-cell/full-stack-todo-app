6b5c4138831d04a127ec5ba007cc54cb411e16df
winchester-cell <manili.mm91@gmail.com>
2025-09-25 11:40:22 +0330

    db changes

--- src/utiles/database/dbConnect.js
+++ src/utiles/database/dbConnect.js
@@ -1,38 +1,38 @@
-import mongoose from "mongoose";
-
-const dbConnect = async () => {
-  try {
-    if (mongoose.connections[0].readyState) {
-      return false
-    } else {
-      await mongoose.connect('mongodb://localhost:27017/TodoList')
-      console.log('Connected to DataBase');
-    }
-  } catch (err) {
-    console.log('Error =>', err);
-  }
-}
-
-export default dbConnect
-
-///////////////////////////////////////////////////////////////////////////////
-
 // import mongoose from "mongoose";
 
 // const dbConnect = async () => {
-//   if (mongoose.connections[0].readyState) {
-//     return;
-//   }
-
 //   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("✅ Connected to MongoDB Atlas");
+//     if (mongoose.connections[0].readyState) {
+//       return false
+//     } else {
+//       await mongoose.connect('mongodb://localhost:27017/TodoList')
+//       console.log('Connected to DataBase');
+//     }
 //   } catch (err) {
-//     console.error("❌ MongoDB connection error:", err);
+//     console.log('Error =>', err);
 //   }
-// };
+// }
 
-// export default dbConnect;
+// export default dbConnect
+
+///////////////////////////////////////////////////////////////////////////////
+
+import mongoose from "mongoose";
+
+const dbConnect = async () => {
+  if (mongoose.connections[0].readyState) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+  }
+};
+
+export default dbConnect;
